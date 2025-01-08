cbe8048f58a65a4452d6cbd6d6a8e5e6ecd5dfcd
Marcos Fernandes <felipefernandes101@hotmail.com>
2025-01-08 12:31:56 -0300

    Adding HATEOAS in person API and unit tests for person services

---
+++ 09_AddHateoas/rest-with-spring-boot-erudio/src/test/java/br/com/erudio/RestWithSpringBootErudioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.erudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringBootErudioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
